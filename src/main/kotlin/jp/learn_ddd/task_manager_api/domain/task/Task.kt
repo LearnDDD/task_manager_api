be845ecd2c793cefa53872e0dc8086bdd5d4474c
s_tomita <s_tomita@s-tomitanoMacBook-Pro.local>
2020-05-31 20:19:26 +0900

    modify task class

--- src/main/kotlin/jp/learn_ddd/task_manager_api/domain/task/Task.kt
+++ src/main/kotlin/jp/learn_ddd/task_manager_api/domain/task/Task.kt
@@ -1,12 +1,12 @@
 package jp.learn_ddd.task_manager_api.domain.task
 
-data class TaskId(var value: Int) {
+data class TaskId(val value: Int) {
 }
 
-data class Task(var id: TaskId) {
+data class Task(val id: TaskId) {
 }
 
-data class Tasks(var list: List<Task>) {
+data class Tasks(val list: List<Task>) {
 }
 
 interface TaskRepository {
