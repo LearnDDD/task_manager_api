c860c84b999247d4e5018cb2dda3fc098481fdd5
Yuki Roppongi <az0.mint9@gmail.com>
2020-05-31 16:50:59 +0900

    initialize commit

---
+++ src/main/kotlin/jp/learn_ddd/task_manager_api/task_manager_api/TaskManagerApiApplication.kt
@@ -0,0 +1,11 @@
+package jp.learn_ddd.task_manager_api.task_manager_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TaskManagerApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<TaskManagerApiApplication>(*args)
+}
---
+++ src/test/kotlin/jp/learn_ddd/task_manager_api/task_manager_api/TaskManagerApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package jp.learn_ddd.task_manager_api.task_manager_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TaskManagerApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
