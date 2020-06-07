e37de6bbf457a5721374becd0eaaf079e02dcada
Yuki Roppongi <az0.mint9@gmail.com>
2020-06-07 11:51:37 +0900

    ヘルスチェックにDATASOURCEの情報も追加

--- src/main/kotlin/jp/learn_ddd/task_manager_api/presentation/controller/HealthController.kt
+++ src/main/kotlin/jp/learn_ddd/task_manager_api/presentation/controller/HealthController.kt
@@ -11,8 +11,17 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/")
 class HealthController {
     @GetMapping
-    fun get(): ResponseEntity<String> {
+    fun get(): ResponseEntity<Response> {
         val headers = HttpHeaders()
-        return ResponseEntity("Application Running.", headers, HttpStatus.OK)
+        return try {
+            val datasource = System.getenv("DATASOURCE")
+            val response = Response("Application Running.", datasource)
+            ResponseEntity(response, headers, HttpStatus.OK)
+        }catch (e: Exception) {
+            val response = Response("Application Failed.", "Unkown")
+            ResponseEntity(response, headers, HttpStatus.INTERNAL_SERVER_ERROR)
+        }
     }
 }
+
+data class Response(val state: String, val datasource: String)
