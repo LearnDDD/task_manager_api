9de0836341da096d125907306e884fff398af5fb
Yuki Roppongi <az0.mint9@gmail.com>
2020-06-10 22:49:21 +0900

    ローカル実行が正常にできるように修正

--- tools/docker/db/mysql_data/1_create_table.sql
+++ tools/docker/db/mysql_data/1_create_table.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS task (
   id int(10) AUTO_INCREMENT NOT NULL,
   title varchar(64) NOT NULL,
   details varchar(256),
-  taskTypeID int(10),
-  parentTaskID int(10),
+  taskTypeId int(10),
+  parentTaskId int(10),
   PRIMARY KEY (id)
 );
